1899b9af0655e1408aace5a71dcd408ffc8e7240
Akio <58257252+lakarimori@users.noreply.github.com>
2026-06-03 18:07:28 -0300

    Add: search-for-word - submission-4

---
+++ Data Structures & Algorithms/search-for-word/submission-4.java
@@ -0,0 +1,49 @@
+/*
+[["A","B","C","E"]
+,["S","F","E","S"]
+,["A","D","E","E"]]
+
+*/
+class Solution {
+    boolean[][] visited;
+
+    public boolean exist(char[][] board, String word) {
+        int row = board.length;
+        int column = board[0].length;
+        visited = new boolean[row][column];
+
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<column; j++) {
+                if (!visited[i][j] && board[i][j] == word.charAt(0)) {
+                    if (searchWord(board, word, i, j, 0)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean searchWord(char[][] board, String word, int row, int column, int position) {
+        if (row < 0 || column < 0 || row >= board.length || column >= board[0].length 
+        || board[row][column] != word.charAt(position) || visited[row][column]) {
+            return false;
+        }
+
+        visited[row][column] = true;
+
+        if (position == word.length() - 1) {
+            return true;
+        }
+
+        boolean res = searchWord(board, word, row, column-1, position+1) 
+        || searchWord(board, word, row, column+1, position+1)  
+        || searchWord(board, word, row-1, column, position+1) 
+        || searchWord(board, word, row+1, column, position+1); 
+
+        visited[row][column] = false;
+
+        return res;
+    }
+}
